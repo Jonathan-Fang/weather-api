2a57465a57099eeb2f85dcf2b6036345f8d2dd90
Jonathan Fang <im.jonathan.fang@gmail.com>
2024-11-01 02:11:36 -0700

    10.31 halloween session

--- app/weather-api.py
+++ app/weather-api.py
@@ -1,12 +1,17 @@
 # Import required libraries
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
 
 # Notes
 # https://console.cloud.google.com/apis/credentials/key/c8751f47-8eec-4042-a17a-8c6db94dec87?project=geocode-api-395201
 # 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
 
 # ask what city they want the weather for and take user input
-location = input('Where do you want to know the weather for?\n') # \n newline, line break
+# location = input('Where do you want to know the weather for?\n') # \n newline, line break
+location = os.getenv("LOCATION")
 
 # turn the user input, feed it into geocode, return latitude and longitude
 get_link = "https://geocode.maps.co/search?q=" + location
@@ -75,3 +80,5 @@ checkpoint = forecast_response_output["properties"]["periods"][0]
 weather_prompt = location + "'s weather: "
 
 print(weather_prompt + checkpoint["detailedForecast"]) # success
+
+# store the city and the value, add these two things to the db 10/31/2024, sqlalchemy stuff
